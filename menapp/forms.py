e4072786cd4b4ff76ae9780323585f163833071c
MasterJayanX <jandres.yanez12@gmail.com>
2022-11-02 20:37:12 -0300

    Update forms.py

--- menapp/forms.py
+++ menapp/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-#recordar usar el makemirations y el migrate
+#recordar usar el makemigrations y el migrate
 
 
 
