27e568380ef232a4ad60009d3e78954f953f9137
Koffy04 <jaimedonosoveloso@gmail.com>
2022-11-07 21:47:48 -0300

    Creacion del cuestionario
    Se hizo todo el cuestionario en su totalidad, con las redirecciones y tambien cambios con el codigo css!

--- menapp/views.py
+++ menapp/views.py
@@ -30,7 +30,39 @@ def cuestionario(request):
 
 def pregunta2(request):
     return render(request,'cuestionariol/pregunta2.html')
+def pregunta3(request):
+    return render(request,'cuestionariol/pregunta3.html')
+def pregunta4(request):
+    return render(request,'cuestionariol/pregunta4.html')
+def pregunta5(request):
+    return render(request,'cuestionariol/pregunta5.html')
+def pregunta6(request):
+    return render(request,'cuestionariol/pregunta6.html')
+def pregunta7(request):
+    return render(request,'cuestionariol/pregunta7.html')
 
+def pri(request):
+    return render(request,'cuestionariol/pri.html')
+def se(request):
+    return render(request,'cuestionariol/se.html')
+def deba(request):
+    return render(request,'cuestionariol/deba.html')
+def acti(request):
+    return render(request,'cuestionariol/acti.html')
+def imp(request):
+    return render(request,'cuestionariol/imp.html')
+def mapa(request):
+    return render(request,'cuestionariol/mapa.html')
+def resu(request):
+    return render(request,'cuestionariol/resu.html')
+def hoja(request):
+    return render(request,'cuestionariol/hoja.html')
+def pomodoro(request):
+    return render(request,'cuestionariol/pomodoro.html')
+def inv(request):
+    return render(request,'cuestionariol/inv.html')
+def flas(request):
+    return render(request,'cuestionariol/flas.html')
 #def cronometro(request):
     if request.method =="POST":
         form = CronometroForm(request.POST)
